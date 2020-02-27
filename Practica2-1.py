db8eec1d117db06ca13395bf4c5fd543a9b7a284
Mario Huerta <mXrio404@localhost.localdomain>
2020-02-27 12:50:30 -0500

    Listo

---
+++ Practica2-1.py
@@ -0,0 +1,25 @@
+import os
+
+red = "200.33.171.0/24"
+os.system("nmap -sP " + red)
+"""
+[mXrio404@localhost VisualProjects]$ /usr/bin/python /home/mXrio404/Documents/VisualProjects/Test1/Practica2-1.py
+Starting Nmap 7.80 ( https://nmap.org ) at 2020-02-27 12:43 EST
+Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+Host is up (0.00083s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.00079s latency).
+Nmap scan report for libra.itmorelia.edu.mx (200.33.171.54)
+Host is up (0.00051s latency).
+Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+Host is up (0.00056s latency).
+Nmap scan report for 200.33.171.85
+Host is up (0.0012s latency).
+Nmap scan report for 200.33.171.120
+Host is up (0.0013s latency).
+Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+Host is up (0.00072s latency).
+Nmap scan report for 200.33.171.127
+Host is up (0.0013s latency).
+Nmap done: 256 IP addresses (8 hosts up) scanned in 12.23 seconds
+[mXrio404@localhost VisualProjects]$ """
